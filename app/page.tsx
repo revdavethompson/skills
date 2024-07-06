86325ab134d3c5283f4232c8a22e261ffc4405aa
Rev. Dave Thompson <davidandrewthompson@gmail.com>
2024-07-05 22:27:03 -0700

    resized icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
 export default function Home() {
   return (
     <div className="flex flex-row flex-wrap justify-center items-center h-screen">
-      <button className="bg-violet-500 flex items-center p-2 rounded-full text-white hover:bg-violet-400">
+      <button className="bg-violet-500 flex items-center justify-center w-16 h-16 rounded-full text-white hover:bg-violet-400 m-2">
         <span className="material-symbols-outlined text-4xl">construction</span>
       </button>
-      <button className="bg-violet-500 flex items-center p-2 rounded-full text-white hover:bg-violet-400">
+      <button className="bg-violet-500 flex items-center justify-center w-16 h-16 rounded-full text-white hover:bg-violet-400 m-2">
         <span className="material-symbols-outlined text-4xl">piano</span>
       </button>
+      <button className="bg-violet-500 flex items-center justify-center w-16 h-16 rounded-full text-white hover:bg-violet-400 m-2">
+        <span className="material-symbols-outlined text-4xl">music_note</span>
+      </button>
+      <button className="bg-violet-500 flex items-center justify-center w-16 h-16 rounded-full text-white hover:bg-violet-400 m-2">
+        <span className="material-symbols-outlined text-4xl">plumbing</span>
+      </button>
+      <button className="bg-violet-500 flex items-center justify-center w-16 h-16 rounded-full text-white hover:bg-violet-400 m-2">
+        <span className="material-symbols-outlined text-4xl">hardware</span>
+      </button>
+      <button className="bg-violet-500 flex items-center justify-center w-16 h-16 rounded-full text-white hover:bg-violet-400 m-2">
+        <span className="material-symbols-outlined text-4xl">piano</span>
+      </button>
+      <button className="bg-violet-500 flex items-center justify-center w-16 h-16 rounded-full text-white hover:bg-violet-400 m-2">
+        <span className="material-symbols-outlined text-4xl">music_note</span>
+      </button>
+      <button className="bg-violet-500 flex items-center justify-center w-16 h-16 rounded-full text-white hover:bg-violet-400 m-2">
+        <span className="material-symbols-outlined text-4xl">plumbing</span>
+      </button>
+      <button className="bg-violet-500 flex items-center justify-center w-16 h-16 rounded-full text-white hover:bg-violet-400 m-2">
+        <span className="material-symbols-outlined text-4xl">hardware</span>
+      </button>
     </div>
-
   );
 }
